2f05afb2236be032506c08a3fd2826b6796f4a64
Nhan Tran <nhantrantnt@gmail.com>
2017-10-08 00:45:03 +0000

    fixed dropbox

--- dropbox_support.py
+++ dropbox_support.py
@@ -2,7 +2,7 @@ import dropbox, sys, os
 import config
 
 dbx = dropbox.Dropbox(config.dropbox_key)
-project_dir = '/home/xilinx/jupyter_notebooks/nhan_dev/snapback1.0'
+project_dir = '/home/xilinx/jupyter_notebooks/nhan_dev/snapback1/capture_moments'
 print('Attempting to upload...')
 
 if not os.path.exists(project_dir):
@@ -14,7 +14,7 @@ for dir, dirs, files in os.walk(project_dir):
 			file_path = os.path.join(dir,file)
 			dest_path = os.path.join('/SnapBack',file)
 			print('Uploading {} to {}'.format(file_path, dest_path))
-			with open(file_path) as f:
+			with open(file_path, 'rb') as f:
 				dbx.files_upload(f.read(), dest_path, mute = True)
 		except Exception as err:
 			print("Failed to upload {}\n{}".format(file, err))
